d751559b9348b6e08f190aaef7afb1fde3a3bfc0
syedshabihhasan <shabih007>
2017-07-26 23:37:43 -0500

    some basic info

---
+++ src/preprocessing.py
@@ -0,0 +1,47 @@
+'''
+@author:Syed Shabih Hasan
+@date: July 26, 2017
+
+This file tries to reduce some of the data from the alcohol csv
+'''
+
+'''
+TODO: read each line using csv, remove the specific locations, also create a dictionary with store number and address
+use the address columns to create the address and not the one column with the composite address
+remove the extract address but keep the zip code
+
+0 Invoice/Item Number,
+1 Date,Store Number,
+2 Store Name,
+3 Address,
+4 City,
+5 Zip Code,
+6 Store Location,
+7 County Number,
+8 County,
+9 Category,
+10 Category Name,
+11 Vendor Number,
+12 Vendor Name,
+13 Item Number,
+14 Item Description,
+15 Pack,
+16 Bottle Volume (ml),
+17 State Bottle Cost,
+18 State Bottle Retail,
+19 Bottles Sold,
+20 Sale (Dollars),
+21 Volume Sold (Liters),
+22 Volume Sold (Gallons)
+
+'''
+
+
+def remove_fields(data_line):
+    # TODO
+    return
+
+
+def read_and_format_csv(filename):
+    # TODO
+    return
